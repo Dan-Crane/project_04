4ef4e9aad93356a2c1e03680975cb188442bc4bb
dani <gre4kaDISCOUNT@gmail.com>
2020-04-23 05:34:45 +0300

    'services'

--- src/js/services/apiService.js
+++ src/js/services/apiService.js
@@ -1,3 +1,22 @@
-// cities
-// prices
-// countries
+import config from "../config/apiConfig";
+
+class Api {
+	constructor(config) {
+		this.url = config.url
+	}
+	async countries (){
+		const res = await fetch(`${this.url}/countries`).then(res=> res.json())
+		return res
+	}
+	async cities (){
+		const res = await fetch(`${this.url}/cities`).then(res=> res.json())
+		return res
+	}
+	async prices (){
+
+	}
+}
+
+const api = new Api(config)
+
+export default api
